5e5357c0e18f700984e4c08162be9573eea71013
ferris <jake@outracks.com>
2015-03-14 05:36:10 +0100

    started getting some elementary tests up and running

--- FsOMeta.Compiler/Proto.fsx
+++ FsOMeta.Compiler/Proto.fsx
@@ -1,1 +1,39 @@
-﻿
+﻿#r "../packages/FParsec.1.0.1/lib/net40-client/FParsecCS.dll"
+#r "../packages/FParsec.1.0.1/lib/net40-client/FParsec.dll"
+
+open FParsec
+
+type AstGrammar =
+    {
+        name : string
+    }
+
+let whitespace = spaces
+let ometa = pstring "ometa"
+let identifier' = identifier (new IdentifierOptions())
+let openBrace = pstring "{"
+let closeBrace = pstring "}"
+
+let grammar =
+    ometa .>> whitespace
+    >>. identifier' .>> whitespace
+    .>> openBrace .>> whitespace
+    .>> closeBrace
+    |>> fun name -> { name = name }
+
+let compilationUnit = whitespace >>. grammar .>> whitespace
+
+let parse code =
+    match run compilationUnit code with
+    | Success (result, _, _) -> result
+    | Failure (message, _, _) -> failwith message
+
+let test = @"
+
+ometa ExpRecognizer
+{
+}
+
+"
+
+parse test
